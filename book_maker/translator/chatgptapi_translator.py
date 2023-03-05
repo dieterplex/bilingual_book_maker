b6c6b7cb49d014e2c61a9ee4a07a1924aee110da
mybaseball52 <mybaseball52@gmail.com>
2023-03-06 00:26:26 +0800

    improve translation quality by updating the prompt do more specific tasks

--- book_maker/translator/chatgptapi_translator.py
+++ book_maker/translator/chatgptapi_translator.py
@@ -23,7 +23,7 @@ class ChatGPTAPI(Base):
                     {
                         "role": "user",
                         # english prompt here to save tokens
-                        "content": f"Please help me to translate,`{text}` to {self.language}, please return only translated content not include the origin text",
+                        "content": f"I want you to act as an {self.language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in {self.language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level {self.language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: \n\n{text}",
                     }
                 ],
             )
@@ -46,7 +46,7 @@ class ChatGPTAPI(Base):
                 messages=[
                     {
                         "role": "user",
-                        "content": f"Please help me to translate,`{text}` to {self.language}, please return only translated content not include the origin text",
+                        "content": f"I want you to act as an {self.language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in {self.language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level {self.language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: \n\n{text}",
                     }
                 ],
             )
--- book_maker/translator/gpt3_translator.py
+++ book_maker/translator/gpt3_translator.py
@@ -30,7 +30,9 @@ class GPT3(Base):
     def translate(self, text):
         print(text)
         self.headers["Authorization"] = f"Bearer {self.get_key(self.api_key)}"
-        self.data["prompt"] = f"Please help me to translate，`{text}` to {self.language}"
+        self.data[
+            "prompt"
+        ] = f"I want you to act as an {self.language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in {self.language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level {self.language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations, the text is: \n\n{text}"
         r = self.session.post(self.api_url, headers=self.headers, json=self.data)
         if not r.ok:
             return text
